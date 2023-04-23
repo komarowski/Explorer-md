ddf02df7a350dee78de9ad9c49b947eec53ddf1a
Nikita Komarov <nikkom2518@gmail.com>
2023-04-23 10:37:50 +0600

    Switch to VS from VSCode

--- MarkdownExplorer/Services/RenderService.cs
+++ MarkdownExplorer/Services/RenderService.cs
@@ -10,54 +10,145 @@ namespace MarkdownExplorer.Services
   public class RenderService
   {
     private const string TemplateHTML = "template.html";
-
     private const string GeneratedJS = "treeview.js";
+    private string FolderFrom { get; }
+    private string FolderTo { get; }
+    private string Template { get; }
 
     /// <summary>
-    /// Converting markdown to html and saving html files.
+    /// Service for rendering static content.
     /// </summary>
-    /// <param name="repository">Repository.</param>
-    public static void MarkdownToHtml(Repository repository)
+    /// <param name="appSettings">Application settings.</param>
+    public RenderService(AppSettings appSettings)
     {
-      foreach (var file in repository.MarkdownFilesToUpdate)
-      {
-        var markdown = File.ReadAllText(Path.Combine(repository.FolderFrom, file.SourcePath), Encoding.UTF8);
-        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-        var html = Markdown.ToHtml(markdown, pipeline);
-        var htmlWithTreeView = InsertIntoTemplate(html, file.TargetName);
-        var targetPath = Path.Combine(repository.FolderTo, $"{file.TargetName}.html");
-        File.WriteAllText(targetPath, htmlWithTreeView, Encoding.UTF8);
-      }
+      FolderFrom = appSettings.FolderFrom;
+      FolderTo = appSettings.FolderTo;
 
-      var indexPath = Path.Combine(repository.FolderTo, "index.html");
-      if (!File.Exists(indexPath))
+      if (File.Exists(TemplateHTML))
       {
-        var html = InsertIntoTemplate(StaticContent.Index, "None");
-        File.WriteAllText(indexPath, html);
+        Template = File.ReadAllText(TemplateHTML, Encoding.UTF8);
+      }
+      else
+      {
+        Template = StaticContent.StandardHTMLTemplate;
+        File.WriteAllText(TemplateHTML, Template);
       }
     }
 
     /// <summary>
-    /// Insert into a template.
+    /// Walk a directory tree by using recursion.
     /// </summary>
-    /// <param name="htmlText">Main html text.</param>
-    /// <param name="htmlName">HTML file name.</param>
-    /// <returns></returns>
-    private static string InsertIntoTemplate(string htmlText, string htmlName)
+    /// <param name="root">Root directory.</param>
+    /// <param name="tree">Storing information.</param>
+    /// <returns>Information about the folder structure.</returns>
+    private TreeStructure WalkDirectoryTree(DirectoryInfo root, TreeStructure tree)
     {
-      string template;
-      if (File.Exists(TemplateHTML))
+      FileInfo[]? files = null;
+      try
       {
-        template = File.ReadAllText(TemplateHTML, Encoding.UTF8);
+        files = root.GetFiles("*.*");
       }
-      else
+      catch (UnauthorizedAccessException ex)
       {
-        template = StaticContent.StandardHTMLTemplate;
-        File.WriteAllText(TemplateHTML, template);
+        LogService.WriteLog(ex.Message, LogType.Error);
+      }
+      catch (DirectoryNotFoundException ex)
+      {
+        LogService.WriteLog(ex.Message, LogType.Error);
       }
 
-      template = template.Replace("{@FileName}", htmlName);
-      template = template.Replace("{@RenderBody}", htmlText);
+      if (files is not null)
+      {
+        foreach (FileInfo sourceFile in files)
+        {
+          if (sourceFile.Extension == ".md")
+          {
+            var fileCode = FileService.GetHtmlCode(FolderFrom, sourceFile.FullName);
+            var targetFile = new FileInfo(Path.Combine(FolderTo, $"{fileCode}.html"));
+            if (!targetFile.Exists || sourceFile.LastWriteTimeUtc > targetFile.LastWriteTimeUtc)
+            {
+              tree.AddMarkdownToUpdate(sourceFile.FullName, fileCode);
+            }
+            var title = FileService.GetFileTitle(sourceFile);
+            tree.AddFileNode(fileCode, title);
+          }
+          else
+          {
+            FileService.CopyFile(sourceFile, FolderTo);
+          }
+        }
+
+        DirectoryInfo[] subDirs = root.GetDirectories();
+        foreach (DirectoryInfo dirInfo in subDirs)
+        {
+          var folderName = dirInfo.Name;
+          var folderCode = FileService.GetHtmlCode(FolderFrom, dirInfo.FullName);
+          tree.AddFolderBlockStart(folderCode, folderName);
+          tree = WalkDirectoryTree(dirInfo, tree);
+          tree.AddFolderBlockEnd();
+        }
+      }
+
+      return tree;
+    }
+
+    /// <summary>
+    /// Converting all markdown files to html.
+    /// </summary>
+    public void RenderAllHtml()
+    {
+      var tree = new TreeStructure();
+      tree = WalkDirectoryTree(new DirectoryInfo(FolderFrom), tree);
+      foreach (var markdownFile in tree.MarkdownFilesToGenerate)
+      {
+        RenderHtml(markdownFile.SourcePath, markdownFile.Code);
+      }
+
+      var indexPath = Path.Combine(FolderTo, "index.html");
+      if (!File.Exists(indexPath))
+      {
+        var html = InsertIntoTemplate(StaticContent.IndexHtmlText, "None");
+        File.WriteAllText(indexPath, html);
+      }
+
+      GenerateJS(tree);
+    }
+
+    /// <summary>
+    /// Converting markdown file to html and save.
+    /// </summary>
+    /// <param name="markdownPath">Markdown file path.</param>
+    public void RenderHtml(string markdownPath, string? htmlCode = null)
+    {
+      if (File.Exists(markdownPath))
+      {
+        if (htmlCode is null)
+        {
+          htmlCode = FileService.GetHtmlCode(FolderFrom, markdownPath);
+        }
+
+        using var fileStream = new FileStream(markdownPath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+        using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+        string markdown = streamReader.ReadToEnd();
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+        var html = Markdown.ToHtml(markdown, pipeline);
+        var htmlWithTreeView = InsertIntoTemplate(html, htmlCode);
+        var targetPath = Path.Combine(FolderTo, $"{htmlCode}.html");
+        using var streamWriter = new StreamWriter(targetPath);
+        streamWriter.WriteLine(htmlWithTreeView);
+      }
+    }
+
+    /// <summary>
+    /// Insert html text into a template.
+    /// </summary>
+    /// <param name="htmlText">Main html text.</param>
+    /// <param name="htmlCode">HTML file code.</param>
+    /// <returns>Final html page.</returns>
+    private string InsertIntoTemplate(string htmlText, string htmlCode)
+    {
+      var template = Template.Replace(StaticContent.TemplateFileCode, htmlCode);
+      template = template.Replace(StaticContent.TemplateRenderBody, htmlText);
       return template;
     }
 
@@ -65,11 +156,10 @@ namespace MarkdownExplorer.Services
     /// Generate javascript file with tree view. 
     /// </summary>
     /// <param name="tree">Tree view.</param>
-    /// <param name="repository">Repository.</param>
-    public static void GenerateJS(TreeStructure tree, Repository repository)
+    public void GenerateJS(TreeStructure tree)
     {
-      var jsText = StaticContent.JavaScriptTemplate.Replace("{@TreeView}", tree.Content);
-      var jsPath = Path.Combine(repository.FolderTo, GeneratedJS);
+      var jsText = StaticContent.JavaScriptTemplate.Replace(StaticContent.TemplateTreeView, tree.Content);
+      var jsPath = Path.Combine(FolderTo, GeneratedJS);
       File.WriteAllText(jsPath, jsText, Encoding.UTF8);
     }
   }
